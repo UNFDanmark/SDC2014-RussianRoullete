e3d83e4495d8fcf284a69716e9abb5c2386df2dd
sdc <github@software.unf.dk>
2014-07-15 21:43:38 +0200

    +++++++++

--- src/com/example/roulette/SocialRoulette.java
+++ src/com/example/roulette/SocialRoulette.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class SocialRoulette extends Activity {
 
@@ -15,6 +17,7 @@ public class SocialRoulette extends Activity {
     Button buttonReload;
     Revolver revolver;
     ImageView chamber;
+    LinearLayout mainScreen;
 
     /**
      * Called when the activity is first created.
@@ -27,6 +30,7 @@ public class SocialRoulette extends Activity {
         buttonFire = (Button) findViewById(R.id.buttonFire);
         buttonReload = (Button) findViewById(R.id.buttonReload);
         chamber = (ImageView) findViewById(R.id.imageView);
+        mainScreen = (LinearLayout) findViewById(R.id.mainScreen);
 
         revolver = new Revolver();
         revolver.ctx = getApplicationContext();
@@ -43,6 +47,20 @@ public class SocialRoulette extends Activity {
             @Override
             public void onClick(View v) {
                 revolver.reload();
+            }
+        });
+
+        mainScreen.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        float initialY = event.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return true;
             }
         });
     }
