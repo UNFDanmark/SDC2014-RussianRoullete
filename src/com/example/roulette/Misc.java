52850a685fec504a3bf98565d66779309dd2f142
sdc <github@software.unf.dk>
2014-07-16 17:43:00 +0200

    removed unused test function

--- src/com/example/roulette/Misc.java
+++ src/com/example/roulette/Misc.java
@@ -1,22 +1,10 @@
 package com.example.roulette;
 
 import android.content.Context;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Misc {
     public static void message(Context ctx, String message){
         Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
-    }
-    public static void test(final ImageView image){
-        RotateAnimation anim = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        anim.setInterpolator(new LinearInterpolator());
-        anim.setRepeatCount(Animation.INFINITE);
-        anim.setDuration((long)0.5);
-
-        image.startAnimation(anim);
     }
 }
