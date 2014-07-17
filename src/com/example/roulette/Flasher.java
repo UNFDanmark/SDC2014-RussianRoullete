da99db56de529eadf1bc59271a4e382b5636dbf6
sdc <github@software.unf.dk>
2014-07-17 11:23:49 +0200

    Muzzleflash functionality.

---
+++ src/com/example/roulette/Flasher.java
@@ -0,0 +1,48 @@
+package com.example.roulette;
+
+import android.content.Context;
+import android.os.Handler;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class Flasher {
+    private RelativeLayout flash;
+    private RelativeLayout view;
+    private Toast toast;
+
+
+    public Flasher(Context context) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(-1, -1);
+        flash = new RelativeLayout(context);
+        flash.setLayoutParams(params);
+        flash.setBackgroundColor(0xffffffff);
+        flash.setVisibility(View.VISIBLE);
+
+        view = new RelativeLayout(context);
+        view.setLayoutParams(params);
+        view.addView(flash);
+
+        toast = new Toast(context);
+        toast.setView(view);
+        toast.setGravity(Gravity.FILL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+    }
+
+
+    public final void flash(int count) {
+        toast.show();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toast.cancel();
+            }
+        }, 200);
+    }
+}
